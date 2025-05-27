caffe68bc607d6617037ea7c704ee836d0e0e351
Janriz Libres <libres.janriz@gmail.com>
2025-05-27 21:33:18 +0800

    setup appwrite for trending metrics

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Search from "./components/Search";
 import Spinner from "./components/Spinner";
 import MovieCard from "./components/MovieCard";
 import { useDebounce } from "react-use";
+import { updateSearchCount } from "./appwrite";
 
 const API_BASE_URL = "https://api.themoviedb.org/3";
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
@@ -21,7 +22,13 @@ const App = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [debouncedSearchTerm, setDebouncedSearchTerm] = useState("");
 
-  useDebounce(() => setDebouncedSearchTerm(searchTerm), 500, [searchTerm]);
+  useDebounce(
+    () => {
+      setDebouncedSearchTerm(searchTerm);
+    },
+    500,
+    [searchTerm]
+  );
 
   const fetchMovies = async (query = "") => {
     setIsLoading(true);
@@ -40,6 +47,10 @@ const App = () => {
 
       const data = await response.json();
       setMovies(data.results || []);
+
+      if (query && data.results.length > 0) {
+        await updateSearchCount(query, data.results[0]);
+      }
     } catch (error) {
       console.error("Error fetching movies:", error);
       setErrorMessage("Error fetching movies. Please try again later.");
---
+++ src/appwrite.js
@@ -0,0 +1,37 @@
+import { Client, Databases, ID, Query } from "appwrite";
+
+const PROJECT_ID = import.meta.env.VITE_APPWRITE_PROJECT_ID;
+const DATABASE_ID = import.meta.env.VITE_APPWRITE_DATABASE_ID;
+const COLLECTION_ID = import.meta.env.VITE_APPWRITE_COLLECTION_ID;
+
+const client = new Client()
+  .setEndpoint("https://fra.cloud.appwrite.io/v1")
+  .setProject(PROJECT_ID);
+
+const databases = new Databases(client);
+
+export const updateSearchCount = async (searchTerm, movie) => {
+  try {
+    const result = await databases.listDocuments(DATABASE_ID, COLLECTION_ID, [
+      Query.equal("searchTerm", searchTerm),
+    ]);
+
+    if (result.total > 0) {
+      const document = result.documents[0];
+
+      await databases.updateDocument(DATABASE_ID, COLLECTION_ID, document.$id, {
+        count: document.count + 1,
+      });
+    } else {
+      await databases.createDocument(DATABASE_ID, COLLECTION_ID, ID.unique(), {
+        searchTerm,
+        poster_url: movie.poster_path
+          ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}`
+          : "",
+        movie_id: movie.id,
+      });
+    }
+  } catch (error) {
+    console.error("Error updating search count:", error);
+  }
+};
